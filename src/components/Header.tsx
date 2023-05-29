b3a0c14273d0bb7e3b7b1bee1ce29c59038ac1b9
dwskme <officialsamirkc@gmail.com>
2023-05-29 17:21:12 +0545

    small changes..

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,10 @@ import styles from './Header.module.css'
 const Header = () => {
   return (
       <div className={`${styles.header}`}>
-          <div className={`${styles.header}`}>
             Weather App by <span>Samir KC.</span>
-          </div>
           <SeachBar/>
         </div>
   )
 }
+
 export default Header
